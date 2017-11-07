34408bb5f083fe4f71402ac5d075c0b120774f84
Ganesh Kathiresan <ganesh3597@gmail.com>
2017-11-07 09:50:19 +0530

    Implement search

--- test.c
+++ test.c
@@ -3,10 +3,24 @@
 #include "include/utils.h"
 #include <unistd.h>
 
-void test_test() {
+void test_search(long len, int key) {
 	Btree* tree = BTree_init("tree.dat");
 	
-	int len = 22;
+	Data *records = get_data("tmp/dataset.csv", len);
+	int i;
+	for(i = 0; i < len; i++) {
+		insert(tree, &records[i]);
+	}
+	
+	search(tree, key);
+	
+	return;
+}
+
+void test_test(long len) {
+	
+	Btree* tree = BTree_init("tree.dat");
+	
 	Data *records = get_data("tmp/dataset.csv", len);
 	int i;
 	for(i = 0; i < len; i++) {
@@ -22,7 +36,7 @@ void test_test() {
 	
 	// printf("%d", temp.n);
 	
-	traverse(tree, tree->root);
+	// traverse(tree, tree->root);
 	
 	for(int i = 0; i < len; i++) {
 		search(tree, records[i].key);
@@ -37,13 +51,20 @@ void verbose() {
 
 void run_tests(char** args, int no_of_args) {
     int i = 0;
+	char ptr[10];
 	
 	if(no_of_args == 0) {
 		verbose();
 	}
 	
+	if(!strcmp(args[0], "-s")) {
+        test_search(strtol(args[1], ptr, 10), strtol(args[2], ptr, 10));
+    }
+	
+	// printf("%s", args[1]);
+	// return;
     if(!strcmp(args[0], "-t")) {
-        test_test();
+        test_test(strtol(args[1], ptr, 10));
     }
 }
 
